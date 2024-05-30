d7732cdb6bc540edf70517e023bd38557490d4ce
Joe Bowser <bowserj@gmail.com>
2024-05-29 18:11:49 -0700

    Forgot to add alt text

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <Image src="/assets/icons/noun-campground-209.svg" height={64} width={64} />
-      <h1 className="text-6xl md:text-6xl font-bold tracking-tighter leading-tight">
+      <Image src="/assets/icons/noun-campground-209.svg" height={64} width={64} alt="Home" />
+      <h1 className="text-6xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-4">
         infil00p.org
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
